5c5a02f97753f1b531ed384b03b1dff9d5ba3e9d
Daniel Yeh <daniel548604106@gmail.com>
2023-01-02 21:40:25 +0800

    feat: add pwa functionality and required sources

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,137 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="O.HI.O Official" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="O.HI.O Official" />
+        <meta
+          name="description"
+          content="The best ecommerce provider for all your needs"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/icons/maskable_icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/icons/icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="/icons/icon-512x512.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/icons/icon-48x48.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+          href="/icons/icon-72x72.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/maskable_icon.png" color="#ffffff" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+
+        {/* <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta
+          name="twitter:image"
+          content="https://yourdomain.com/icons/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@daniel548604106" /> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="O.HI.O Official" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://o-hi-o-next13.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://o-hi-o-next13.vercel.app/icons/maskable_icon.png"
+        />
+
+        {/* <!-- apple splash screen images --> */}
+        {/* https://appsco.pe/developer/splash-screens */}
+        <link
+          href="/splashscreens/iphone5_splash.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/iphone6_splash.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/iphoneplus_splash.png"
+          media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/ipad_splash.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/ipadpro1_splash.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/ipadpro3_splash.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splashscreens/ipadpro2_splash.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ pages/_offline.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Offline = () => {
+  return <div>Oops you are offline! Please check your network connection</div>;
+};
+
+export default Offline;
