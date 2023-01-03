e98a137cba45fc2aeb1bdc405a1ccd818d729c86
Daniel Yeh <daniel548604106@gmail.com>
2023-01-03 13:41:56 +0800

    feat: i18n setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n,
 };
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
 });
 
 module.exports = withPWA(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import Header from '@/components/Header';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
+import Header from "@/components/Header";
+
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
@@ -16,4 +18,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </>
   );
-}
+};
+
+export default appWithTranslation(App);
