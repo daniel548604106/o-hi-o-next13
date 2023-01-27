5357182b78132bcc8e2d37c67081b8625add5a5c
Daniel Yeh <daniel548604106@gmail.com>
2023-01-27 17:41:11 +0800

    fix: fix type

--- redux/slices/siteSlice.ts
+++ redux/slices/siteSlice.ts
@@ -21,20 +21,9 @@ export const siteSlice = createSlice({
   name: "site",
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
-  reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
-    },
-  },
+  reducers: {},
 });
 
-export const { increment, decrement, incrementByAmount } = siteSlice.actions;
+export const {} = siteSlice.actions;
 
 export default siteSlice.reducer;
